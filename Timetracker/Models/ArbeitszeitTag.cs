53b7c73b1b1d4a2f6d2cbf76293d0efe5892f5e4
Andreas Neumann <andreas.neumann86@gmail.com>
2025-03-25 21:54:40 +0100

    initial create

---
+++ Timetracker/Models/ArbeitszeitTag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timetracker.Models
+{
+    public class ArbeitszeitTag
+    {
+        public DateTime Datum { get; set; }
+        public TimeSpan Start { get; set; }
+        public TimeSpan Ende { get; set; }
+        public TimeSpan Pause { get; set; }
+        public string Notiz { get; set; }
+    }
+}
